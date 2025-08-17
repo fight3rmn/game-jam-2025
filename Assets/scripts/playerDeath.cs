b56c2b87f8c4766c77529e7456aceef46bd015e7
fight3rmn <benballard2000@gmail.com>
2025-08-17 04:07:19 -0700

    added map

--- Assets/scripts/playerDeath.cs
+++ Assets/scripts/playerDeath.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class playerDeath : MonoBehaviour
 {
     public int hp;
-    void onCollisionEnter2D(Collision2D hit) {
+    void OnCollisionEnter2D(Collision2D hit) {
         if(hit.gameObject.tag.Equals("Lazer")) {
             hp--;
             if(hp <= 0) {
